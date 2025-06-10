bf4a522964c053a93e9fe9d21609a7bcb5a9e6af
hieu <hieuschooler@gmail.com>
2025-06-10 21:38:42 +0700

    t

--- src/main/java/org/example/doantn/Security/WebConfig.java
+++ src/main/java/org/example/doantn/Security/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Áp dụng cho tất cả các endpoint
-                .allowedOrigins("http://localhost:4200")  // Đảm bảo rằng chỉ có frontend của bạn có thể truy cập API
+                .allowedOrigins("https://fedatn-r6fd.vercel.app")  // Đảm bảo rằng chỉ có frontend của bạn có thể truy cập API
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Các phương thức HTTP được phép
                 .allowedHeaders("*")  // Cho phép tất cả các headers
                 .allowCredentials(true);  // Nếu cần gửi cookies hoặc authorization headers
